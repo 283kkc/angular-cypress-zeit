9f5ff963b6068d25b1522a1118e042550d53cbb7
283kkc <pictures1682@gmail.com>
2019-10-09 17:43:22 +0900

    index.js dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+
+const app = express();
+
+app.use('/admin', basicAuth({
+  challenge: true,
+  realm: 'now-basic-auth-node-express',
+  users: { 'admin': 'admin' },
+  unauthorizedResponse: 'Restricted area. Please login (admin:admin).'
+}));
+app.use(express.static(__dirname + '/*'));
+
+app.listen(4444, () => console.log('Listening on port 4444...'));
