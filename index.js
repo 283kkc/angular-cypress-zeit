664c6b44e857ba2d5e0155dd76903a38b7f7d7a9
283kkc <pictures1682@gmail.com>
2019-10-10 10:49:09 +0900

    fix index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const basicAuth = require('express-basic-auth');
 
 const app = express();
 
-app.use('/', basicAuth({
+app.use('/index.html', basicAuth({
   challenge: true,
   realm: 'now-basic-auth-node-express',
   users: { 'admin': 'admin' },
   unauthorizedResponse: 'Restricted area. Please login (admin:admin).'
 }));
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(4444, () => console.log('Listening on port 4444...'));
